fixed jacobian calculation and changed to jacobian of transformed points rather than displacement.

transformation_graph.py
@@ -665,11 +665,13 @@ def apply_transformation(adj, spaces, src_space, src_img, dest_space, out, src_p
 
         emlddmm.write_vtk_data(output_name, xI, disp, title)
 
-        # write out determinant of jacobian (detjac) of displacement
+        # write out determinant of jacobian (detjac) of the transformed coordinates
         dv = [(x[1]-x[0]).to('cpu') for x in xI]
-        grad = np.stack(np.gradient(disp[0], 1, dv[0], dv[1], dv[2]), axis=-1)
-        grad = np.reshape(grad, grad.shape[:-1]+(2,2))
-        detjac = np.linalg.det(grad)
+        jacobian = lambda X,dv : np.stack((np.stack(np.gradient(X[0,0], dv[0], dv[1], dv[2]), axis=-1), 
+                                           np.stack(np.gradient(X[0,1], dv[0], dv[1], dv[2]), axis=-1),
+                                           np.stack(np.gradient(X[0,2], dv[0], dv[1], dv[2]), axis=-1)), axis=-1)
+        J = jacobian(X,dv)
+        detjac = np.linalg.det(J)
         if J_title == 'slice_dataset':
             output_name = os.path.join(transform_dir, f'{dest_space}_to_{src_space}_REGISTERED_detjac.vtk')
             title = f'{dest_space}_to_{src_space}_REGISTERED_detjac'